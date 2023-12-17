a77c121ac1fd8fe24ac14c292fa6c3b74cea87fc
Josep Ocaña <j.ocanapuigdevall@gmail.com>
2023-12-17 17:51:52 +0100

    agregado Bootstrap y cards en el contenedor

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
-const btn = document.querySelector('.btn');
-tituloOculto = document.querySelector('.titulo');
+const btn = document.querySelector('.boton');
+const tituloOculto = document.querySelector('.titulo');
 
 btn.addEventListener('click', descubrir);
 
 function descubrir() {
+	console.log('hola');
+
 	if (tituloOculto.classList.contains('descubrir')) {
 		tituloOculto.classList.remove('descubrir');
 		tituloOculto.classList.add('ocultar');
