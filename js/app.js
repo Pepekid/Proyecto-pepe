d39d273988d10f45212362527ad940e56bc7cb15
Josep Ocaña <j.ocanapuigdevall@gmail.com>
2023-12-17 16:55:01 +0100

    Agregando boton con clases y el javascript

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
-function saludar() {
-	alert('hola a todos');
+const btn = document.querySelector('.btn');
+tituloOculto = document.querySelector('.titulo');
+
+btn.addEventListener('click', descubrir);
+
+function descubrir() {
+	if (tituloOculto.classList.contains('descubrir')) {
+		tituloOculto.classList.remove('descubrir');
+		tituloOculto.classList.add('ocultar');
+		return;
+	}
+	tituloOculto.classList.add('descubrir');
+	tituloOculto.classList.remove('ocultar');
 }
